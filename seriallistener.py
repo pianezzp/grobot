afdbe856fda6fb580e9c11f7b77f305060be1b1c
pianezzp <pianezzi@me.com>
2022-12-16 10:08:36 +0100

    add seriallistener - first commit, yay

---
+++ seriallistener.py
@@ -0,0 +1,15 @@
+from serial import Serial as aserial
+from time import sleep
+
+ser = aserial('/dev/ttyS0',9600 )    #Open port with baud rate
+
+while True:
+    received_data = ser.read()              #read serial port
+    sleep(.03)
+    data_left = ser.inWaiting()             #check for remaining byte
+    received_data += ser.read(data_left)
+    try:
+        print (received_data.decode())                   #print received data
+        ser.write(received_data)
+    except:
+        print(received_data)
