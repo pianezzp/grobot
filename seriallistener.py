082854fed3f4070da937c5c3de7ed37e01e93eef
pianezzp <pianezzi@me.com>
2022-12-21 12:13:22 +0100

    added database support to sercom

--- seriallistener.py
+++ seriallistener.py
@@ -2,17 +2,35 @@
 # Raspberry Pi and Arduino are linked through internal serial port through level shifter
 
 from serial import Serial as aserial
-from time import sleep
+from time import sleep,mktime
+import datetime
+import sqlite3
+from sqlite3 import Error
+
+
+            
+conn = sqlite3.connect('grobo_system.db')  
+print ("Opened database successfully")
+    
+
+
 
 ser = aserial('/dev/ttyS0',9600 )    #Open port with baud rate
 
+
 while True:
+    x=6
+    date_time = datetime.datetime.now()   
     received_data = ser.read()              #read serial port
     sleep(.03)
     data_left = ser.inWaiting()             #check for remaining byte
     received_data += ser.read(data_left)
     try:
-        print (received_data.decode())                   #print received data
+        print (received_data.decode()) #print received data
+        sql_insert=  "INSERT INTO Power (DATE,POWER,CURRENT) VALUES ("+str(mktime(date_time.timetuple()))+","+str(received_data.decode().splitlines()[1])+","+str(received_data.decode().splitlines()[0])+")"
+        conn.execute(sql_insert);  
+        conn.commit()
         ser.write(received_data)
+        x=+1
     except:
         print(received_data)
